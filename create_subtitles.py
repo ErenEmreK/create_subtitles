ded2204970029ffe75425d43afb082c0ca522de6
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-04-30 13:33:09 +0300

    main func update

--- create_subtitles.py
+++ create_subtitles.py
@@ -34,6 +34,7 @@ def result_to_srt(result, output_file, glue):
             subs.append(sub_item)
 
     else:
+        #If glue is used we set end time of instances as start of the next one
         for i in range(len(result['segments']) - 1):
             start_time = convert_time(result['segments'][i]['start'])
             end_time = convert_time(result['segments'][i+1]['start'])
@@ -48,12 +49,10 @@ def result_to_srt(result, output_file, glue):
     subs.save(output_file, encoding='utf-8')
     print(f'Subtitle file {output_file} is ready.')   
 
-def subtitles_for_folder(video_folder, sub_folder, sub_extension='.srt', glue=False):
-    files = os.listdir(video_folder)
-    file_count = len(files)
+def subtitles_for_list(model, video_list, sub_path, OUT_ISDIR=False, sub_extension='.srt', glue=False):
+
     done = 0
-    for file in files:
-        video_path = os.path.join(video_folder, file)
+    for video_path in video_list:
         result = model.transcribe(video_path)
         
         sub_file = os.path.splitext(video_path)[0] + sub_extension
@@ -68,16 +67,30 @@ def subtitles_for_folder(video_folder, sub_folder, sub_extension='.srt', glue=Fa
             break
     
 def get_paths(input, output):
-    if not os.path.exists(input):
-        print(f"Couldn't reach {input}")
+    #TODO handle -e case later
+    #TODO optimize
+    #We return True if output is folder, False if file (to avoid checking it later)
+    OUT_ISDIR = False 
+    
+    for requested_path in (input, output):
+        if not os.path.exists(requested_path):
+            print(f"Couldn't reach {requested_path}")
+            sys.exit()
+    
+    if os.path.isfile(input) and os.path.isfile(output):
+        i, o = [input], output
+        
+    elif os.path.isdir(input) and os.path.isdir(output):
+        OUT_ISDIR = True
+        i, o = [os.path.join(input, file) for file in os.listdir(input)], output
+    
+    else:
+        print("Input and output formats should match. [input_file] [output_file] or [input_folder] [output_folder]")
         sys.exit()
         
-    if os.path.isfile(input):
-        i = [input]
-    elif os.path.isdir(input):
-        i = [os.path.join(input, file) for file in os.listdir(input)]
- 
+    return i, o, OUT_ISDIR
             
 def main():
-    paths = get_paths(sys.argv[1], sys.argv[2])
-     
+    input_paths, output_path, OUT_ISDIR = get_paths(sys.argv[1], sys.argv[2])
+    
+    
