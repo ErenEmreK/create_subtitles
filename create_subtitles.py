552c496086186b1c5df8253c46254189cff1613a
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-05-01 14:11:45 +0300

    wip

--- create_subtitles.py
+++ create_subtitles.py
@@ -17,12 +17,12 @@ def convert_time(seconds):
     
     return (hours, minutes, seconds, miliseconds)
 
-def result_to_srt(result, output_file, glue=False, plus_time=False):
+def result_to_srt(result, output_file, plus_time=0):
     #We create our srt object
     subs = pysrt.SubRipFile()
 
-    if not glue:
-        #If glue method isnt used, we create subtitle instances for every sentence
+    if not plus_time:
+        #If plus-time method isnt used, we create subtitle instances for every sentence
         #in our result, using its start-end timestamps
         for segment in result['segments']:
             start_time = convert_time(segment['start']) 
@@ -34,17 +34,12 @@ def result_to_srt(result, output_file, glue=False, plus_time=False):
             subs.append(sub_item)
 
     else:
-        #If glue is used we set end time of instances as start of the next one
+        #If plus-time is used we set end time of instances as start of the next one
         for i in range(len(result['segments']) - 1):
             start_time = convert_time(result['segments'][i]['start'])
-            if not plus_time:
-                end_time = convert_time(result['segments'][i+1]['start'])
-            else:
-                #TODO drop second calc altogether, use start time above instead
-                #We add plus time to instance to prolong it
-                #TODO check
-                end_time = convert_time(min(result['segments'][i+1]['start'], 
-                                            (result['segments'][i]['end']) + plus_time))
+            end_time = convert_time(min(result['segments'][i+1]['start'], 
+                                        (result['segments'][i]['end']) + plus_time))
+            
             sub_item = pysrt.SubRipItem(start=start_time, 
                                         end=end_time, 
                                         text=result['segments'][i]['text'])
@@ -55,51 +50,63 @@ def result_to_srt(result, output_file, glue=False, plus_time=False):
     subs.save(output_file, encoding='utf-8')
     print(f'Subtitle file {output_file} is ready.')   
 
-def subtitles_for_list(model, video_list, sub_path, OUT_ISDIR=False, sub_extension='.srt', glue=False):
+def subtitles_for_list(model, video_list, sub_path, plus_time=0, sub_extension='.srt'):
     file_count = len(video_list)
     done = 0
     for video_path in video_list:
         result = model.transcribe(video_path)
-        if OUT_ISDIR:
-            #TODO Just get filename w/o absolute path and create sub_file that way
+        if os.path.isdir(sub_path):
             sub_file = os.path.splitext(video_path)[0] + sub_extension
-            sub_path = os.path.join(sub_path, sub_file)
         else:
             sub_file = sub_path
             
         if os.path.splitext(sub_file)[1] == '.srt':
-            result_to_srt(result, sub_path, glue=glue)
+            result_to_srt(result, sub_path, plus_time=plus_time)
             done += 1
             print(f"{done}/{file_count}")
         else:
             #TODO implement other subtitle formats
             print(f"Unable to create {sub_extension} files. Use '.srt' instead.")
             break
+          
+def get_commands(sys_args):
     
-def get_paths(input, output):
-    #TODO handle -e case later
-    #TODO optimize
-    #We return True if output is folder, False if file (to avoid checking it later)
-    OUT_ISDIR = False 
-    
-    for requested_path in (input, output):
-        if not os.path.exists(requested_path):
-            print(f"Couldn't reach {requested_path}")
-            sys.exit()
-    
-    if os.path.isfile(input) and os.path.isfile(output):
-        i, o = [input], output
+    if len(sys_args) >= 2:
         
-    elif os.path.isdir(input) and os.path.isdir(output):
-        OUT_ISDIR = True
-        i, o = [os.path.join(input, file) for file in os.listdir(input)], output
+        output_path = None
+        plus_time = 0
+        
+        extensions = ['.mp4', '.mkv', '.mp3', '.wav', 'mpeg', 'm4a', 'webm']
+        
+        i = sys_args[1]
+        
+        if os.path.isfile(i):
+            input = [i]
+        
+        elif os.path.isdir(i):
+            input = [os.path.join(i, file) for file in os.listdir(i) if os.path.splitext(file)[1] in extensions]
     
+        else:
+            print("Input file/directory is invalid.")
+            sys.exit()
+            
+        if "-o" in sys_args:    
+            try:
+                output_path = sys_args[sys_args.index("-o") + 1]
+            except IndexError:
+                pass
+    
+        if not output_path:
+            output_path = 
+            
     else:
-        print("Input and output formats should match. [input_file] [output_file] or [input_folder] [output_folder]")
+        print("You must enter an input path.")
         sys.exit()
         
-    return i, o, OUT_ISDIR
-            
+
+    
+    
+    
 def main():
     #1-Assuming you can use "." in CLI: 
     model_names = ['tiny.en', 'base.en', 'small.en', 'medium.en', 'large.en', 
