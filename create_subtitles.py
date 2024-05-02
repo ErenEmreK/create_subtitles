736827a97df7362e85bafa0a97bd88b052170cea
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-05-02 20:17:57 +0300

    e

--- create_subtitles.py
+++ create_subtitles.py
@@ -99,53 +99,57 @@ def get_commands(sys_args):
             input_list = [os.path.join(i, file) for file in os.listdir(i) if os.path.splitext(file)[1] in extensions]
     
         else:
-            print("Input file/directory is invalid.")
+            print(f"Couldn't reach {i}")
             sys.exit()
             
-        if "-o" in sys_args:    
-            try:
-                requested_path = sys_args[sys_args.index("-o") + 1] 
-                if os.path.isdir(requested_path) :
-                    output_path = requested_path
-                elif os.path.isfile(requested_path) and os.path.splitext(requested_path)[1] in sub_extensions:
-                    print(os.path.splitext(requested_path)[1])
-                    output_path = requested_path
-                    sub_format = os.path.splitext(requested_path)[1]
-                else:
-                    print("Requested output path is invalid or includes non-supported format. Supported formats: " + str(sub_extensions))
-                    sys.exit()
-            #TODO
-            except IndexError:
-                pass
+        for n in len(range(sys_args)):
+            if sys_args[n] == "-o":
+                try:
+                    requested_path = sys_args[n + 1] 
+                    if os.path.isdir(requested_path):
+                        output_path = requested_path
+                    elif os.path.isfile(requested_path) and os.path.splitext(requested_path)[1] in sub_extensions:
+                        print(os.path.splitext(requested_path)[1])
+                        output_path = requested_path
+                        sub_format = os.path.splitext(requested_path)[1]
+                    else:
+                        print("Requested output path is invalid or includes non-supported format. Supported formats: " + str(sub_extensions))
+                        sys.exit()
+                
+                except IndexError:
+                    print("Output location isn't defined. Creating the files in video folder instead. ")
+                    pass
+                
+            if sys_args[n] == "-f":
+                try:
+                    requested_format = sys_args[n + 1] 
+                    if requested_format in sub_extensions:
+                        sub_format = requested_format
+                    else:
+                        print("Unable to create requested subtitle format. Supported formats: " + str(sub_extensions))
+                        sys.exit()
+                except IndexError:
+                    pass
     
-        if "-f" in sys_args:
-            try:
-                requested_format = sys_args[sys_args.index("-f") + 1] 
-                if requested_format in sub_extensions:
-                    sub_format = requested_format
-                else:
-                    print("Unable to create requested subtitle format. Supported formats: " + str(sub_extensions))
+            if sys_args[n] == "-p":
+                try:
+                    plus_time = int(n + 1) 
+                except (ValueError, IndexError):
+                    print("Plus-time didn't specified properly.")
                     sys.exit()
-            except IndexError:
-                pass
             
-        if "-p" in sys_args:
-            try:
-                plus_time = int(sys_args[sys_args.index("-p") + 1]) 
-            except (ValueError, IndexError):
-                print("Plus-time didn't specified properly.")
-                sys.exit()
-            
-        if "-m" in sys_args:
-            model_size_names = ['tiny.en', 'base.en', 'small.en', 'medium.en', 'large.en', 
-                   'tiny', 'base', 'small', 'medium', 'large']
-            try:
-                requested_model = sys_args[sys_args.index("-m") + 1] 
-                if requested_model in model_size_names:
-                    model_size = requested_model
-            except IndexError:
-                print(f"Model keyword did not used properly. Default ({model_size}) model is being used instead.")
-                pass
+            if sys_args[n] == "-m":
+                model_size_names = ['tiny.en', 'base.en', 'small.en', 'medium.en', 'large.en', 
+                                    'tiny', 'base', 'small', 'medium', 'large']
+                try:
+                    requested_model = sys_args[n + 1] 
+                    if requested_model in model_size_names:
+                        model_size = requested_model
+                except IndexError:
+                    print("Model keyword did not used properly. ")
+                    print("Possible model keywords: " + str(model_size_names))
+                    sys.exit()
+        
             
         return model_size, input_list, output_path, sub_format, plus_time
     
